f9c901a3f7d9d195fb1d6aebb6b6f42eff2a38e0
Mori <mobileabi7@gmail.com>
2022-04-24 21:19:15 +0430

    Tas added

---
+++ Tas.py
@@ -0,0 +1,15 @@
+#tas
+# اگر همه مردم  دنیا تاس بندازند / میانگین اینکه دفعه چندم شش اومده چیست ؟
+import random
+
+lst= []
+count = 0
+for i in range (1000000) :
+    count +=1
+    tas = random.randint(1,6)
+
+    if tas == 6 :
+        lst.append(count)
+        count = 0
+# print(lst)
+print(sum(lst)/len(lst))
